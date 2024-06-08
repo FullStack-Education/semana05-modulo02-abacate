72c32d2367b518eaae675bf6d7df397b129f5554
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-07 22:52:35 -0300

    refactoring student data confirmation

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 function obterDadosAluno() {
-    let nome = window.prompt("Qual o seu nome?")
-    let idade = window.prompt("Qual a sua idade?")
-    let serie = window.prompt("Qual a sua série?")
-    let escola = window.prompt("Qual o nome da sua escola?")
-    let materia = window.prompt("Qual a sua matéria favorita?")
+    let nome, idade, serie, escola, materia;
 
-    let confirmação = window.confirm("Vocé confirma os dados informados?")
+    while (!nome) {
+        nome = window.prompt("Qual o seu nome?");
+        if (nome === null) return; 
+    }
+    do {
+        idade = window.prompt("Qual a sua idade?");
+        if (idade === null) return; 
+        idade = Number(idade); 
+    } while (isNaN(idade) || idade < 0 || idade > 140);
+
+    while (!serie) {
+        serie = window.prompt("Qual a sua série?");
+        if (serie === null) return; 
+    }
+    while (!escola) {
+        escola = window.prompt("Qual o nome da sua escola?");
+        if (escola === null) return;
+    }
+    while (!materia) {
+        materia = window.prompt("Qual a sua matéria favorita?");
+        if (materia === null) return;
+    }
+
+    let confirmação = window.confirm("Você confirma os dados informados?");
 
     if (confirmação) {
-        let nomeAluno = document.getElementById("nome-aluno")
-        let idadeAluno = document.getElementById("idade-aluno")
-        let serieAluno = document.getElementById("serie-aluno")
-        let escolaAluno = document.getElementById("escola-aluno")
-        let materiaAluno = document.getElementById("materia-aluno")
-
-        nomeAluno.innerHTML = nome
-        idadeAluno.innerHTML = idade
-        serieAluno.innerHTML = serie
-        escolaAluno.innerHTML = escola
-        materiaAluno.innerHTML = materia
+        document.getElementById("nome-aluno").innerHTML = nome;
+        document.getElementById("idade-aluno").innerHTML = idade;
+        document.getElementById("serie-aluno").innerHTML = serie;
+        document.getElementById("escola-aluno").innerHTML = escola;
+        document.getElementById("materia-aluno").innerHTML = materia;
     } else {
-        window.alert("Os dados não foram confirmados.")
+        window.alert("Os dados não foram confirmados.");
     }
 }
 
-obterDadosAluno()
+obterDadosAluno();
